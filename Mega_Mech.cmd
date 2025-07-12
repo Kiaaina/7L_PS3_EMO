f3b1f4a851c6d2c2acf6e967dc8cc4534e1a41d4
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2025-07-12 08:23:26 -1000

    Update Mega_Mech.cmd

--- Mega_Mech.cmd
+++ Mega_Mech.cmd
@@ -6,7 +6,6 @@ color 74
 cls
 
 :top
-start https://mega.nz/folder/JCkknbTZ#gaXn8OCBBH1_F9MoRX0Qwg
 start https://mega.nz/folder/RKtlUDZY#br799ou9TqHjYTH2-FLn3w
 echo Mega_By_Kia7L...
 (timeout /t 5) > nul
